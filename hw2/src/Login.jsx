1faa62a3009756b68c5bff3b4ec3c4187e72fa1a
Jay Trivedi <jaytrivedi@Jays-Air.lan>
2024-03-18 12:40:42 -0400

    Finished end to end testing for logging in

--- hw2/src/Login.jsx
+++ hw2/src/Login.jsx
@@ -3,20 +3,63 @@ import { useState } from 'react';
 
 function Login({setIsLoggedIn}) {
     const [driverLogIn, setDriverLogIn] = useState(false)
+    const [currDriverStage, setCurrDriverStage] = useState('initial')
+    const [driverUserName, setDriverUserName] = useState('')
+
     const handleStudentLogIn = () => {
         setIsLoggedIn(true)
         setDriverLogIn(false)
     }
     const handleDriverLogIn = () => {
-      //alert("Function not available yet")
       setDriverLogIn(true)
+      setCurrDriverStage('username')
     }
+
+    const handleLogInSubmit = (e) => {
+      e.preventDefault()
+      if (driverUserName === 'Driver1') {
+        setCurrDriverStage("driverLoggedIn")
+      }
+    }
+
+    const handleDriverLogout = () => {
+      setDriverUserName('');
+      setDriverLogIn(false)
+      setCurrDriverStage('initial')
+    };
 
   return (
     <>
-        <button onClick={handleStudentLogIn}>Student</button>
-        <button onClick={handleDriverLogIn}>Bus Driver</button>
-        {driverLogIn && <h2>Feature not available yet! Please log in as Student</h2>}
+        {driverLogIn === false ? (
+          <>
+            <button onClick={handleStudentLogIn}>Student</button>
+            <button onClick={handleDriverLogIn}>Bus Driver</button>
+          </>
+        ) : (
+          <>
+            {currDriverStage === 'username' && (
+              <>
+                <h2>For now, simply enter Driver1 as username</h2>
+                <form onSubmit={handleLogInSubmit}>
+                  <label>
+                    Username:
+                    <input type="text" value={driverUserName} onChange={(e) => setDriverUserName(e.target.value)} />
+                  </label>
+                  <button type="submit">Submit</button>
+                </form>
+              </>
+            )}
+            {currDriverStage === 'driverLoggedIn' && (
+              <>
+                <h2>Welcome {driverUserName}! Unfortunately, data rendering is still being worked on.</h2>
+                <h2>However, your account is successfully logged into.</h2>
+                <button onClick={handleDriverLogout}>Log Out</button>
+              </>
+            )}
+
+          </>
+        )
+      }
     </>
   );
 }
