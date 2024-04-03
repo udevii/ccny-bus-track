cc8b115310de47f0537cedeef0e506b4c2d4243c
udevii <httpxurmm@gmail.com>
2024-04-03 18:48:40 -0400

    Exported Navbar onto App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,20 +6,24 @@ import HomePage from './pages/HomePage'
 import Ccny from './pages/CCNY'
 import Street125 from './pages/Street125'
 import Street145 from './pages/Street145'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    
-    <Routes>
-      <Route path={'/'} element={<LoginPage/>}/>
-      <Route path={'/signup'} element={<RegisterPage/>} />
-      <Route path={'/Home'} element={<HomePage/>}/>
-      <Route path={'/CCNY'} element={<Ccny/>}/>
-      <Route path={'/Street125'} element={<Street125/>}/>
-      <Route path={'/Street145'} element={<Street145/>}/>
-    </Routes>
-      
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<LoginPage/>}/>
+        <Route path='/signup' element={<RegisterPage/>} />
+        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/CCNY' element={<Ccny/>}/>
+        <Route path='/Street125' element={<Street125/>}/>
+        <Route path='/Street145' element={<Street145/>}/>
+        {/* Your other routes */}
+      </Routes>
+    </>
   )
 }
 
 export default App
+
--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 const navbar = () => {
   return (
     <div className='body'>
-      <ul class="nav-links">
+      <ul className="nav-links">
         <li><a href="Home.html">Home</a></li>
-        <li class="center"><a href="PublicSafety.html">Public Safety</a></li>
-        <li class="logo"><img src="ccnybuslogo.png" alt="CCNY Bus Logo" /></li> <!-- Logo added here -->
-        <li class="upward"><a href="MyQRCode.html">My QR Code</a></li>
-        <li class="forward"><a href="LoginSignup.html">Login/Signup</a></li>
+        <li className="center"><a href="PublicSafety.html">Public Safety</a></li>
+        <li className="logo"><img src="ccnybuslogo.png" alt="CCNY Bus Logo" /></li> <!-- Logo added here -->
+        <li className="upward"><a href="MyQRCode.html">My QR Code</a></li>
+        <li className="forward"><a href="LoginSignup.html">Login/Signup</a></li>
     </ul>
     </div>
   )
