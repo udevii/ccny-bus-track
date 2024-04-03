07ccd7b83dd840d191425bb4d10923c6aca01080
Jay Trivedi <jaytrivedi@Jays-Air.lan>
2024-04-03 17:56:59 -0400

    Started integrating frontend branch into main

---
+++ hw2/src/pages/Street145.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+// import { Coords125, CoordsCCNY, Coords145 } from './Coordinates';
+// import './Street145.module.css';
+import './Street.css'
+
+function Street145() {
+  const [currPage, setCurrPage] = useState('125')
+
+  const handleHomeClick = () => {
+    setCurrPage('Home')
+  }
+
+  return (
+    <>
+        {currPage === 'Home' &&(
+          <>
+            <Home></Home>
+          </>
+        )}
+        {currPage === '125' &&(
+            <>
+              <div className="App">
+                  <div className='input-container'>
+                      <h2>Your current location is 145 Street Station</h2>
+                      <div className="Street125Container">
+                        <div className="image-container">
+                          <img src="https://www.ccny.cuny.edu/sites/default/files/styles/600px_wide/public/2023-06/ColinPowellSchool_CCNY_Economics%26Business_GuestFaculty_DemetriosPapacostasNoPhoto.png?itok=5r-pw_fF" alt="Street 125" />
+                          <h2>City College</h2>
+                        </div>
+                        <h3>8 minutes away</h3>
+                      </div>
+                      <button onClick={handleHomeClick}>Back to home</button>
+                  </div>
+              </div>
+            </>
+        )}
+    </>
+  )
+}
+
+export default Street145
